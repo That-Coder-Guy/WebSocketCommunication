527cfa4c3dcd60cd95e98832f321f59416f648a9
Henry Glenn <hgglenn518@gmail.com>
2025-02-16 22:41:58 -0600

    Finished removing unneeded code

--- TestServer/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using TestServer;
-using WebSocketCommunication.Server;
-
-WebSocketServer server = new WebSocketServer(8080);
-server.AddService<Chat>("/chat/");
-server.Start();
-Console.ReadKey();
